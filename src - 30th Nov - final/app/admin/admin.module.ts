14a68191a611e8dcae822be7390b3eabf4c41ed0
MohamedHalludeen <43991681+MohamedHalludeen@users.noreply.github.com>
2018-11-30 18:21:09 +0530

    Add files via upload

---
+++ src - 30th Nov - final/app/admin/admin.module.ts
@@ -0,0 +1,74 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+import { AdminComponent } from './adminComponent/admin.component';
+import { AdminMenuComponent } from './adminMenu/admin-menu.component';
+import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './signUp/sign-up.component';
+import { UserMenuComponent } from './userMenu/user-menu.component';
+import { TruncatePipe } from './adminShared/trunc.pipe';
+
+import { environment } from '../../environments/environment';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
+import { UserService } from './adminShared/user.service';
+import { IncidentService } from './adminShared/incident.service';
+import { IncidentComponent } from './incident/incident.component';
+import { IncidentCreateComponent } from './incidentCreate/incident-create.component';
+import { changeservice } from './adminShared/change.service';
+import { changecomponent } from './change/change.component';
+import { changeCreateComponent } from './changecreate/change-create.component';
+import { releasecomponent } from './release-deployment/release-deployment.component';
+
+const AdminRoutes: Routes = [
+    {
+        path: 'admin',
+        component: AdminComponent,
+        children: [
+            { path: 'incidents', component: IncidentComponent, canActivate: [UserService]},
+            { path: 'change', component: changecomponent , canActivate: [UserService]},
+            { path: 'login', component: LoginComponent },
+            { path: 'signup', component: SignUpComponent },
+            { path: 'user', component: UserMenuComponent, canActivate: [UserService] },
+            { path: 'release', component: releasecomponent, canActivate: [UserService] },
+            { path: '', component: AdminMenuComponent, canActivate: [UserService] }
+        ]
+    },
+];
+
+@NgModule({
+    imports: [
+        CommonModule,
+        FormsModule,
+        RouterModule.forChild(AdminRoutes),
+        AngularFireModule.initializeApp(environment.firebase),
+        AngularFireDatabaseModule
+    ],
+    exports: [
+        RouterModule,
+        TruncatePipe
+    ],
+    declarations: [
+        AdminComponent,
+        AdminMenuComponent,
+        UserMenuComponent,
+        LoginComponent,
+        SignUpComponent,
+        IncidentComponent,
+        IncidentCreateComponent,
+        TruncatePipe,
+        changecomponent,
+        changeCreateComponent,
+        releasecomponent
+    ],
+    providers: [
+        UserService,
+        IncidentService,
+        changeservice
+        
+    ]
+})
+export class AdminModule {}
